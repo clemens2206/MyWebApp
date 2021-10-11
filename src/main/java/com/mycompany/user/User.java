2f0109974d85e54f92c263345692ac31258e8569
clemens2206 <clekerber@tsn.at>
2021-10-11 23:26:08 +0200

    Commit G fertig UserRepositoryTests

--- src/main/java/com/mycompany/user/User.java
+++ src/main/java/com/mycompany/user/User.java
@@ -61,4 +61,15 @@ public class User {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", passwort='" + passwort + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
